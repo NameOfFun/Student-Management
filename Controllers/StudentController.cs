95686599b64e996560854344bd4a36352051347a
NameOfFun <12damhao@gmail.com>
2026-03-02 23:29:24 +0700

    edit student, validate form request

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -51,5 +51,57 @@ namespace StudentManagement.Controllers
                                         }).ToList();
             return View(student);
         }
+
+        //Edit a Student
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _context.Students.FirstOrDefaultAsync(s => s.StudentId == id);
+
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Department = _context.Departments.Select(d => new SelectListItem
+            {
+                Value = d.DepartmentId.ToString(),
+                Text = d.DepartmentName
+            }).ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingStudent = await _context.Students
+            .FirstOrDefaultAsync(s => s.StudentId == student.StudentId);
+
+                if (existingStudent == null)
+                    return NotFound();
+
+                // update fields manually
+                existingStudent.FullName = student.FullName;
+                existingStudent.DepartmentId = student.DepartmentId;
+                existingStudent.Email = student.Email;
+                existingStudent.Phone = student.Phone;
+                existingStudent.Address = student.Address;
+                existingStudent.DateOfBirth = student.DateOfBirth;
+                existingStudent.Gender = student.Gender;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Department = _context.Departments.Select(d => new SelectListItem
+            {
+                Value = d.DepartmentId.ToString(),
+                Text = d.DepartmentName,
+                Selected = d.DepartmentId == student.DepartmentId
+            }).ToList();
+            return View(student);
+        }
     }
 }
